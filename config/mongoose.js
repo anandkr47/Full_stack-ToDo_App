20a44a9013301ab565e72712d9864ca3a95610c2
Anand kumar <96875322+anandkr47@users.noreply.github.com>
2023-03-31 23:17:42 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/todo_list_db');
+mongoose.connect('mongodb://localhost:27017/todo_list_db');
 const db = mongoose.connection;
 // error
 
