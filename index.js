4838d2e5f0ad4cf2c06d7b4755c4939abe80c204
Anand kumar <anandkumar.triden@gmail.com>
2023-03-31 20:51:10 +0530

    update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const expressLayout = require('express-ejs-layouts');
 app.use(expressLayout)
 const db = require('./config/mongoose')
 //const TodoLists = require('./models/todo_list')
-// it is use to handle middle ware here we are using express.urlenceode to us e the parser
+// it is use to handle middle ware here we are using express.
 
 app.use(express.urlencoded()) 
 app.use('/',require('./routes'))
